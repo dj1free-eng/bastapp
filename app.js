7166c8c149529762f907d33354458517c173db4c
dj1free-eng <dj1free@gmail.com>
2026-01-08 23:01:08 +0000

    Update app.js

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ function renderWheel(){
   lettersLayerEl.innerHTML = '';
   const N = LETTERS.length;
   const center = { x: 50, y: 50 };
-  const radius = 38;
+  const radius = 35;
   const startAngleDeg = -90;
 
   LETTERS.forEach((letter, i) => {
